1d22ef4c565f511eb7603af550c5edcd747d93a1
Bonze255 <fragger255@hotmail.com>
2022-01-04 20:18:17 +0100

    Add files via upload

--- clocktwo/qclocktwo.js
+++ clocktwo/qclocktwo.js
@@ -4,22 +4,28 @@ $.widget("sv.qclocktwo_qclocktwo", $.sv.widget, {
 	initSelector: '[data-widget="qclocktwo.qclocktwo"]',
 
 	options: {
-		time: 360,
+		time: 120,
 		mode: 'widget',
 		lang: 'de',
+		////color: 'white', 
 	},
 
 	_create: function () {
 		this._super();
 		console.log("qlocktwo");
 		console.log("timeout is ", this.options.time);
+		// create user defined shadow
+		
+		//$("lit").css("color", 'green');
+		//$("lit").css("text-shadow", '0 0 10px '+this.options.color);
 
 		//idleTimer() takes an optional argument that defines the idle timeout
 		//timeout is in milliseconds; defaults to 30000
 		if (this.options.mode == 'screensaver') {
 			console.log("Function screensaver");
-			idleTimer(this.options.time);
+			idleTimer(this.options.time*1000);
 			console.log("starte timer");
+
 			function showQlock() {
 				console.log("showqlock");
 				$(".mask").css('display','block');
@@ -42,22 +48,18 @@ $.widget("sv.qclocktwo_qclocktwo", $.sv.widget, {
 			});
 		} else {
 			console.log("Function widget");
-			//$("#qclocktwo").css("font-size", "1.8em");
 			$("#qclocktwo").css("display", "flex");
-			//$('.q2col').css('height', '');
-			//$('.frame').css('height', '');
-			//$("#qclocktwo").css("line-height", "1.8em");
-			//$('.overlay').css('height', '100vh');
 			$('.frame').addClass('widget');
 		}
-		//var qclockid = 'qclock';
+		
+
+
+
 
 		clearInterval(q2.clockTimer);
 		abortFadeTimer();
 		q2.current.minute = -1;
 
-
-		
 		initQlockTwo('de', '#qclocktwo');
 		q2.current.litCells = [0, 0, 0, 0];
 		changeCells(currentTimeCells());
